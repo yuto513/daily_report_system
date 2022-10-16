bac46794925c9a75c74902b019411fff9105b7e0
yuto.inoue <inoue.yuto0513@gmail.com>
2022-10-16 12:37:56 +0900

    createアクションの作成

--- src/main/java/actions/ActionBase.java
+++ src/main/java/actions/ActionBase.java
@@ -16,11 +16,10 @@ import constants.ForwardConst;
 import constants.PropertyConst;
 
 /**
- * 各Actionクラスの親クラス。共通処理を行う
+ * 各Actionクラスの親クラス。共通処理を行う。
  *
  */
 public abstract class ActionBase {
-
     protected ServletContext context;
     protected HttpServletRequest request;
     protected HttpServletResponse response;
@@ -32,15 +31,17 @@ public abstract class ActionBase {
      * @param servletRequest
      * @param servletResponse
      */
-    public void init(ServletContext servletContext, HttpServletRequest servletRequest, HttpServletResponse servletResponse){
+    public void init(
+            ServletContext servletContext,
+            HttpServletRequest servletRequest,
+            HttpServletResponse servletResponse) {
         this.context = servletContext;
         this.request = servletRequest;
         this.response = servletResponse;
     }
 
-
     /**
-     * フロントコントローラーから呼び出されるメソッド
+     * フロントコントローラから呼び出されるメソッド
      * @throws ServletException
      * @throws IOException
      */
@@ -51,10 +52,12 @@ public abstract class ActionBase {
      * @throws ServletException
      * @throws IOException
      */
-    protected void invoke() throws ServletException, IOException{
+    protected void invoke()
+            throws ServletException, IOException {
 
         Method commandMethod;
         try {
+
             //パラメータからcommandを取得
             String command = request.getParameter(ForwardConst.CMD.getValue());
 
@@ -71,6 +74,7 @@ public abstract class ActionBase {
             //commandの値が不正で実行できない場合エラー画面を呼び出し
             forward(ForwardConst.FW_ERR_UNKNOWN);
         }
+
     }
 
     /**
@@ -83,10 +87,11 @@ public abstract class ActionBase {
 
         //jspファイルの相対パスを作成
         String forward = String.format("/WEB-INF/views/%s.jsp", target.getValue());
-        RequestDispatcher despatcher = request.getRequestDispatcher(forward);
+        RequestDispatcher dispatcher = request.getRequestDispatcher(forward);
 
         //jspファイルの呼び出し
-        despatcher.forward(request, response);
+        dispatcher.forward(request, response);
+
     }
 
     /**
@@ -96,32 +101,38 @@ public abstract class ActionBase {
      * @throws ServletException
      * @throws IOException
      */
-    protected void redirect(ForwardConst action, ForwardConst command) throws ServletException, IOException {
+    protected void redirect(ForwardConst action, ForwardConst command)
+            throws ServletException, IOException {
+
         //URLを構築
-        String redirectUrl = request.getContextPath() +"&command=" + action.getValue();
-        if(command != null) {
+        String redirectUrl = request.getContextPath() + "/?action=" + action.getValue();
+        if (command != null) {
             redirectUrl = redirectUrl + "&command=" + command.getValue();
         }
 
         //URLへリダイレクト
         response.sendRedirect(redirectUrl);
+
     }
 
     /**
      * CSRF対策 token不正の場合はエラー画面を表示
-     * @return
+     * @return true: token有効 false: token不正
      * @throws ServletException
      * @throws IOException
      */
     protected boolean checkToken() throws ServletException, IOException {
+
         //パラメータからtokenの値を取得
         String _token = getRequestParam(AttributeConst.TOKEN);
 
-        if(_token == null || !(_token.equals(""))) {
-            //tokenが設定されていない or セッションIDと一致しない場合はエラー画面を表示
+        if (_token == null || !(_token.equals(getTokenId()))) {
+
+            //tokenが設定されていない、またはセッションIDと一致しない場合はエラー画面を表示
             forward(ForwardConst.FW_ERR_UNKNOWN);
+
             return false;
-        }else {
+        } else {
             return true;
         }
 
@@ -137,27 +148,27 @@ public abstract class ActionBase {
 
     /**
      * リクエストから表示を要求されているページ数を取得し、返却する
-     * @return 要求されているページ数(要求がない場合は１)
+     * @return 要求されているページ数(要求がない場合は1)
      */
     protected int getPage() {
         int page;
         page = toNumber(request.getParameter(AttributeConst.PAGE.getValue()));
-        if(page == Integer.MIN_VALUE) {
+        if (page == Integer.MIN_VALUE) {
             page = 1;
         }
-    return page;
+        return page;
     }
 
     /**
      * 文字列を数値に変換する
-     * @param strNumber 交換前文字列
+     * @param strNumber 変換前文字列
      * @return 変換後数値
      */
-    private int toNumber(String strNumber) {
+    protected int toNumber(String strNumber) {
         int number = 0;
         try {
             number = Integer.parseInt(strNumber);
-        }catch(Exception e) {
+        } catch (Exception e) {
             number = Integer.MIN_VALUE;
         }
         return number;
@@ -169,7 +180,7 @@ public abstract class ActionBase {
      * @return 変換後LocalDateインスタンス
      */
     protected LocalDate toLocalDate(String strDate) {
-        if(strDate == null || strDate.equals("")){
+        if (strDate == null || strDate.equals("")) {
             return LocalDate.now();
         }
         return LocalDate.parse(strDate);
@@ -185,7 +196,7 @@ public abstract class ActionBase {
     }
 
     /**
-     * リクエストスコープにパラメータを設定
+     * リクエストスコープにパラメータを設定する
      * @param key パラメータ名
      * @param value パラメータの値
      */
@@ -194,9 +205,9 @@ public abstract class ActionBase {
     }
 
     /**
-     * セッションスコープから指定されたパラメータの値を取得して返却
+     * セッションスコープから指定されたパラメータの値を取得し、返却する
      * @param key パラメータ名
-     * @return パラメータ名
+     * @return パラメータの値
      */
     @SuppressWarnings("unchecked")
     protected <R> R getSessionScope(AttributeConst key) {
@@ -229,4 +240,5 @@ public abstract class ActionBase {
     protected <R> R getContextScope(PropertyConst key) {
         return (R) context.getAttribute(key.getValue());
     }
+
 }
